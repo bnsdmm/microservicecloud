9741e94526d0f38cd83a148f17814bdfbd55a3f5
wangzhen <506826727@qq.com>
2018-07-09 16:34:57 +0800

    自定义轮询，实现每个服务被调用5次

--- microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/DeptConsumer80_APP.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/DeptConsumer80_APP.java
@@ -1,11 +1,14 @@
 package com.atguigu.springcloud;
 
+import com.atguigu.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient // 本服务启动后会自动注册进eureka服务中
+@RibbonClient(name="MICROSERVICECLOUD-DEPT",configuration=MySelfRule.class)
 public class DeptConsumer80_APP {
 	public static void main(String[] args) {
 		SpringApplication.run(DeptConsumer80_APP.class, args);
